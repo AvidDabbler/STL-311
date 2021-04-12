d50df9f66d6d7175063aaab0d387b32063aee4d3
AvidDabbler <walter.k.jenkins@gmail.com>
2021-04-11 21:58:30 -0500

    form logic and start of reverse geocode

--- front/components/NewRequest.js
+++ front/components/NewRequest.js
@@ -4,22 +4,24 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import TextField from '@material-ui/core/TextField';
 import GpsFixedIcon from '@material-ui/icons/GpsFixed';
 import CheckIcon from '@material-ui/icons/Check';
+import { Button } from '@material-ui/core';
+import { loadModules } from 'esri-loader';
 
 
-const DropdownField = ({list, obj, setFunction, desc}) => {
-    return(
+const DropdownField = ({ list, obj, setFunction, desc }) => {
+    return (
         <>
             <Select
                 // className={classes.selectEmpty}
                 name={desc}
                 label={desc}
-                onChange={e=>setFunction(e.target.value)}
-                // inputProps={{ 'aria-label': 'age' }}
-                >
+                onChange={e => setFunction(e.target.value)}
+            // inputProps={{ 'aria-label': 'age' }}
+            >
                 <option value="" disabled>
                     Placeholder
                 </option>
-                {list.map(el=> <option value={el}>{el}</option>)}
+                {list.map(el => <option value={el}>{el}</option>)}
             </Select>
             <FormHelperText>{desc}</FormHelperText>
         </>
@@ -27,7 +29,7 @@ const DropdownField = ({list, obj, setFunction, desc}) => {
 }
 
 
-export default function NewRequest({ formVisability, dropdown}) {
+export default function NewRequest({ formVisability, dropdown }) {
     // Problem Dropdown
     const [probList, setProbList] = useState([])
     const [prob, setProb] = useState('')
@@ -38,16 +40,20 @@ export default function NewRequest({ formVisability, dropdown}) {
 
     // Location Description
     const [locDesc, setLocDesc] = useState('')
-    
+
     // Long Description
     const [longDesc, setLongDesc] = useState('')
 
     // GPS Coordinates
     const [loc, setLoc] = useState('')
+    const [geocode, setGeocode] = useState('')
 
     // Photo Attachment
-    const [photo, setPhoto] = useState('')
+    // const [photo, setPhoto] = useState('')
 
+    let key = process.env.NEXT_PUBLIC_ESRI_API_KEY
+
+    
 
     // inititial load
     useEffect(() => {
@@ -59,14 +65,35 @@ export default function NewRequest({ formVisability, dropdown}) {
     }, [dropdown])
 
     // dropdown change
-    useEffect(()=>{
+    useEffect(() => {
         prob ? setDescList(Object.keys(dropdown[prob])) : ''
     }, [prob, desc])
 
-    const getCoord = (e) => {        
+    useEffect(()=>{
+        reverseGeo(loc)
+    }, [loc])
+
+    const reverseGeo = (coor) =>{
+        loadModules([
+            "esri/config",
+            "esri/tasks/locator"
+            ])
+        .then(function ([Locator]){
+            esriConfig.apiKey = key
+            const geocodeurl = 'https://maps6.stlouis-mo.gov/arcgis/rest/services/GEOCODERS/COMPOSITE_GEOCODE/GeocodeServer'
+            const locator = new Locator(geocodeurl);
+            locator.locationToAddress({location: coor})
+        })
+        .then(address =>{
+            setGeocode(address)
+        }).catch(error=>console.log(error))
+        
+    }
+
+    const getCoord = (e) => {
 
         function success(position) {
-            const latitude  = position.coords.latitude;
+            const latitude = position.coords.latitude;
             const longitude = position.coords.longitude;
             setLoc(`${latitude},${longitude}`)
         }
@@ -75,7 +102,20 @@ export default function NewRequest({ formVisability, dropdown}) {
             status.textContent = 'Unable to retrieve your location';
         }
 
-        navigator.geolocation.getCurrentPosition(success, error);
+        navigator.geolocation.getCurrentPosition(success, error)
+
+    }
+
+    const clickHandle = () => {
+        if (prob == '' ||
+            desc == '' ||
+            locDesc == '' ||
+            longDesc == '' ||
+            loc == '') {
+            return;
+        }
+        let twitterUrl = `https://twitter.com/intent/tweet?screen_name=stscb&ref_src=twsrc%5Etfw&text=%0AProblem:${prob}%0ADesc:${desc}%0ALoc Desc:${locDesc}%0ALongDesc:${longDesc}%0Address:${geocode}`
+        window.open(twitterUrl, '_blank');
     }
 
 
@@ -87,23 +127,34 @@ export default function NewRequest({ formVisability, dropdown}) {
                     <form className='form'>
                         <DropdownField list={probList} obj={dropdown} setFunction={setProb} desc='Problem Category' />
                         {descList.length > 1 ? <DropdownField list={descList} obj={dropdown[prob]} setFunction={setDesc} desc='Problem Description' /> : ''}
-                        <TextField id="standard-basic" label="Location Description" onChange={e=>setLocDesc(e.target.value)} />
+                        <TextField
+                            id="standard-basic"
+                            label="Location Description"
+                            required={true}
+                            onChange={e => setLocDesc(e.target.value)} />
                         <TextField
                             id="standard-multiline-flexible"
                             label="Long Description"
                             multiline
                             rows={4}
-                            onChange={e=>setLongDesc(e.target.value)}
-                            />
-                        <input type="file" style={{ paddingTop: '20px'}} />
+                            required={true}
+                            onChange={e => setLongDesc(e.target.value)}
+                        />
+                        {/* <input type="file" style={{ paddingTop: '20px' }} /> */}
                         <div className='row' style={{ padding: '20px' }}>
                             <GpsFixedIcon
                                 color="primary" style={{ fontSize: 50, paddingRight: '20px' }}
-                                onClick={ e=> getCoord(e) }
-                                />
+                                onClick={e => getCoord(e)}
+                            />
                             <p style={{ paddingRight: '20px' }}>Share your location </p>
-                            {loc ? <CheckIcon style={{ fontSize: 50, paddingRight: '20px', color: 'green'  }} /> : ''}
+                            {loc ? <CheckIcon style={{ fontSize: 50, paddingRight: '20px', color: '#F78243' }} /> : ''}
                         </div>
+                        <Button
+                            type='submit'
+                            style={{ width: '100%', height: '30px', backgroundColor: 'red' }}
+                            onClick={() => clickHandle()}>
+                            Tweet it
+                    </Button>
                     </form>
                 </div> :
                 ''
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
 
 import dropdown from '../data/dropdown.json'
 
